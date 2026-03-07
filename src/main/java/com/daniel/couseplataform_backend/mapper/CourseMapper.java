c41a0090cdd6caa7d99f7dd243fbcbae0fc80101
dnielbr <everaldodaniel@hotmail.com>
2026-03-07 09:27:12 -0300

    add a method toEntity to mapper of Course

--- src/main/java/com/daniel/couseplataform_backend/mapper/CourseMapper.java
+++ src/main/java/com/daniel/couseplataform_backend/mapper/CourseMapper.java
@@ -1,5 +1,6 @@
 package com.daniel.couseplataform_backend.mapper;
 
+import com.daniel.couseplataform_backend.dto.request.CourseCreateRequestDto;
 import com.daniel.couseplataform_backend.dto.response.CourseResponseDto;
 import com.daniel.couseplataform_backend.dto.response.CourseSummaryDto;
 import com.daniel.couseplataform_backend.model.Course;
@@ -9,6 +10,8 @@ import java.util.List;
 
 @Mapper(componentModel = "spring", uses = CourseModuleMapper.class)
 public interface CourseMapper {
+
+    Course toEntity(CourseCreateRequestDto dto);
 
     CourseResponseDto toCourseDto(Course course);
     CourseSummaryDto toDto(Course course);
