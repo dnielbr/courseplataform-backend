45c5b1aaf516addfc53e252c2e094d253fff6ffe
dnielbr <everaldodaniel@hotmail.com>
2026-03-07 09:40:00 -0300

    add pageNumber to PageDto and correcting hte word 'fist' to 'first'

--- src/main/java/com/daniel/couseplataform_backend/dto/response/PageDto.java
+++ src/main/java/com/daniel/couseplataform_backend/dto/response/PageDto.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 public record PageDto<T>(
         List<T> content,
+        int page,
         int size,
         long totalElements,
         int totalPages,
         boolean last,
-        boolean fist,
+        boolean first,
         boolean hasNext,
         boolean hasPrevious
 ) {
